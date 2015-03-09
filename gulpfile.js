6e13d169a84c7d198b5f9cf6deef6db1e9b57c12
MikhakMisaghian <MikhakMisaghian@users.noreply.github.com>
2015-03-08 20:48:27 -0700

    Added gulpfile.js resized two images(img/mobilewebdev.jpg  views/images/pizzeria.jpg) using preview tools on my mac ran gulp imagemin command and it minified two images and saved them in build/views/images/pizza.png build/views/images/pizzeria.jpg

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+
+// minify new images
+gulp.task('imagemin', function() {
+	var imgSrc = './img/**/*',
+	imgDst = './build/img';
+
+	gulp.src(imgSrc)
+		.pipe(changed(imgDst))
+		.pipe(imagemin())
+		.pipe(gulp.dest(imgDst));
+});
+
+// minify new images
+gulp.task('imagemin', function() {
+	var imgSrc = './views/images/**/*',
+	imgDst = './build/views/images';
+
+	gulp.src(imgSrc)
+		.pipe(changed(imgDst))
+		.pipe(imagemin())
+		.pipe(gulp.dest(imgDst));
+});
